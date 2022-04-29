7542e1c521b1c5543102ecc4579ae09bc563e77d
Pragnesh Patel <46986245+pragneshp7@users.noreply.github.com>
2022-04-28 21:17:41 -0500

    made changes to hls files

--- hls/hls_mixcolumn.c
+++ hls/hls_mixcolumn.c
@@ -1,18 +1,25 @@
-void aes_mix_columns(uint8_t state[16])
+#include "hls_mixcolumn.h"
+void aes_mix_columns(uint8_t state[16], uint8_t state_out[16])
 {
+#pragma HLS INTERFACE s_axilite port=return
+#pragma HLS RESOURCE variable=state_out core=ROM_1P_BRAM
+#pragma HLS INTERFACE s_axilite port=state_out
+#pragma HLS RESOURCE variable=state core=ROM_1P_BRAM
+#pragma HLS INTERFACE s_axilite port=state
+
     uint8_t y[16] = { 2, 3, 1, 1,  1, 2, 3, 1,  1, 1, 2, 3,  3, 1, 1, 2};
     uint8_t s[4];
     int i, j, r;
    
-    for (i = 0; i < 4; i++) {
-        for (r = 0; r < 4; r++) {
+    L1: for (i = 0; i < 4; i++) {
+        L2 :for (r = 0; r < 4; r++) {
             s[r] = 0;
-            for (j = 0; j < 4; j++) {
+            L3: for (j = 0; j < 4; j++) {
                 s[r] = s[r] ^ ((((y[r * 4 + j] >> 0) & 1) * state[i * 4 + j]) ^ (((y[r * 4 + j] >> 1) & 1) * ((state[i * 4 + j] << 1) ^ (((state[i * 4 + j] >> 7) & 1) * 0x1b))) );
             }
         }
-        for (r = 0; r < 4; r++) {
-            state[i * 4 + r] = s[r];
+        L4: for (r = 0; r < 4; r++) {
+            state_out[i * 4 + r] = s[r];
         }
     }
 }
---
+++ hls/hls_mixcolumn.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+void aes_mix_columns(uint8_t state[16], uint8_t state_out[16]);
