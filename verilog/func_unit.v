1dc68ed28d1ff14496d616748b1ed7f4fd799f44
Pragnesh Patel <46986245+pragneshp7@users.noreply.github.com>
2022-05-04 20:42:27 -0500

    added func_unit. Need to add control module

--- verilog/func_unit.v
+++ verilog/func_unit.v
@@ -25,8 +25,51 @@ output done
 );
 
 reg [1:0] y [15:0];
+reg [7:0] s [15:0];
+reg [7:0] input_state [15:0];
+wire [7:0] sin;
+wire [7:0] state_in;
+wire [7:0] state_out;
+wire [1:0] yin;
 
-// uint8_t y[16] = { 2, 3, 1, 1,  1, 2, 3, 1,  1, 1, 2, 3,  3, 1, 1, 2};
+always(posedge clk) begin
+if (reset) begin
+	input_state[0] <= 8'b0;
+	input_state[1] <= 8'b0;
+	input_state[2] <= 8'b0;
+	input_state[3] <= 8'b0;	
+	input_state[4] <= 8'b0;
+	input_state[5] <= 8'b0;
+	input_state[6] <= 8'b0;
+	input_state[7] <= 8'b0;	
+	input_state[8] <= 8'b0;
+	input_state[9] <= 8'b0;
+	input_state[10] <= 8'b0;
+	input_state[11] <= 8'b0;	
+	input_state[12] <= 8'b0;
+	input_state[13] <= 8'b0;
+	input_state[14] <= 8'b0;
+	input_state[15] <= 8'b0;		
+end
+else if (start) begin
+	input_state[0] <= state0[7:0];
+	input_state[1] <= state0[15:8];
+	input_state[2] <= state0[23:16];
+	input_state[3] <= state0[31:24];	
+	input_state[4] <= state1[7:0];
+	input_state[5] <= state1[15:8];
+	input_state[6] <= state1[23:16];
+	input_state[7] <= state1[31:24];	
+	input_state[8] <= state2[7:0];
+	input_state[9] <= state2[15:8];
+	input_state[10] <= state2[23:16];
+	input_state[11] <= state2[31:24];	
+	input_state[12] <= state3[7:0];
+	input_state[13] <= state3[15:8];
+	input_state[14] <= state3[23:16];
+	input_state[15] <= state3[31:24];
+end
+end
 
 initial begin
 y[0] = 2'd2;
@@ -47,10 +90,40 @@ y[14] = 2'd1;
 y[15] = 2'd2;
 end
 
-wire [7:0] sin;
-wire [7:0] state_in;
-wire [7:0] state_out;
-wire [1:0] yin;
+always(posedge clk) begin
+if (reset || start) begin
+	s[0] <= 8'b0;
+	s[1] <= 8'b0;
+	s[2] <= 8'b0;
+	s[3] <= 8'b0;	
+	s[4] <= 8'b0;
+	s[5] <= 8'b0;
+	s[6] <= 8'b0;
+	s[7] <= 8'b0;	
+	s[8] <= 8'b0;
+	s[9] <= 8'b0;
+	s[10] <= 8'b0;
+	s[11] <= 8'b0;	
+	s[12] <= 8'b0;
+	s[13] <= 8'b0;
+	s[14] <= 8'b0;
+	s[15] <= 8'b0;	
+end
+else begin
+	s[xyz] <= state_out; //TO-DO same as assign sin
+end
+end
+
+// uint8_t y[16] = { 2, 3, 1, 1,  1, 2, 3, 1,  1, 1, 2, 3,  3, 1, 1, 2};
+assign state_out0 = {s[3],s[2],s[1],s[0]};
+assign state_out1 = {s[7],s[6],s[5],s[4]};
+assign state_out2 = {s[11],s[10],s[9],s[8]};
+assign state_out3 = {s[15],s[14],s[13],s[12]};
+
+assign state_in = input_state[xyz]; //TO-DO
+assign y_in = y[xyz]; //TO-DO
+assign sin = s[xyz]; //TO-DO
+
 
 
 control dut_control();
